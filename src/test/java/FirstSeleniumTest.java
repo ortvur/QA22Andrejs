5c835491675ebbfb3bf82215f6eb44cb6f5f5401
ortvu <stalinjugend@gmail.com>
2022-11-02 23:03:39 +0200

    Test with mistake for you:)

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -2,17 +2,24 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.time.Instant;
 
 public class FirstSeleniumTest {
     @Test
+    private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
     public void openHomeCheck() {
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
         WebDriver browser = new ChromeDriver();
         browser.get("http://discovercars.com/");
 
-        By.id("element-id");
-        By.name("input-field-name");
-        By.xpath(".//label[@for = 'is-drop-off']");
+        WebDriverWait = new WebDriverWait(browser, Duration.ofSeconds(10);
+
+        wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
+        browser.findElement(ACCEPT_COOKIES_BTN).click();
 
 
 
