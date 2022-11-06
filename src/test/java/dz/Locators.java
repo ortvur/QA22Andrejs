8cba5fd801021efef9b894b0ca6bb2e386035c72
ortvu <stalinjugend@gmail.com>
2022-11-07 01:13:32 +0200

    Locators for u

---
+++ src/test/java/dz/Locators.java
@@ -0,0 +1,51 @@
+package dz;
+
+public class Locators {
+
+<div class="supplier-rating">8.8</div>
+
+
+            .//*[contains(@class, 'logo-cell')];
+            .//*[contains(@class, 'choose-currency')];
+            .//*[contains(@class, 'my-booking')];
+            .//*[contains(@class, 'home-caption')];
+            .//*[contains(@class, 'sb-residence-text')];
+            .//*[contains(@class, 'date-field trigger-gtm-sb hasDatepicker active')];
+            .//*[contains(@class, 'button button-green')];
+            .//*[contains(@class, 'arrow-slider-shape')]
+            .//*[contains(@class, 'row is-table-row')]
+                    .//*[contains(@class, 'top-row')]
+            .//*[contains(@class, 'img small')]
+                    .//*[contains(@class, 'showing-cars')]
+            .//*[contains(@class, 'car-top-filter-control next')]
+
+                    .//*span[text() = '1 bag']
+            .//*[contains(@class, 'category-label trend')]
+            .//*[contains(@class, 'min-price')]
+            .//*[contains(@class, 'filter-fuel-type')]['gasoline']
+                    .//*[contains(@class, 'filter-number-of-seats')]['4 seats']
+            .//*[contains(@class, 'selected-car')]['btn-book']['prevent-loading']['btn-book']
+            .//*[contains(@class, 'rental-conditions')]
+            .//*[contains(@class, 'supplier-rating')]
+            .//*[contains(@class, 'price-item-price-main')]
+            .//*[contains(@class, 'supplier-rating')]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
