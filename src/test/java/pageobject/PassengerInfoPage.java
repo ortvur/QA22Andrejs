6778d104095d6dc844aa93409b1faba957c9e763
ortvu <stalinjugend@gmail.com>
2023-01-24 02:12:10 +0200

    dnt to make type

--- src/test/java/pageobject/PassengerInfoPage.java
+++ src/test/java/pageobject/PassengerInfoPage.java
@@ -1,6 +1,10 @@
 package pageobject;
 
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class PassengerInfoPage {
 
@@ -15,11 +19,38 @@ public class PassengerInfoPage {
     private final By GET_PRICE_LINK = By.xpath(".//span[@onclick = 'setLang();']");
     private BaseFunc baseFunc;
 
+
+    @Test
+    public void RegistrationTest() {
+
     public PassengerInfoPage(BaseFunc baseFunc) {
         this.baseFunc = baseFunc;
     }
-    public void passengerInfoPage (Passenger passenger){
-        baseFunc.type(FROM, passenger.getFrom());
+
+        private final String URL = "qaguru.lv:8089/tickets/";
+        private WebDriver browser;
+        private WebDriverWait wait;
+
+
+
+
+baseFunc.openUrl(String);
+
+baseFunc.select(By FROM = By.id("afrom");
+baseFunc.select(By TO = By.id("bfrom");
+baseFunc.select( By DESTANATION_FROM = By.xpath(".//span[@class = 'bTxt'][1]"));
+baseFunc.select(By DESTANATION_TO = By.xpath(".//span[@class = 'bTxt'][2]"););
+baseFunc.select(By SELECT_DATE = By.id("flight");
+
+baseFunc.select(By GET_PRICE_LINK = By.xpath(".//span[@onclick = 'setLang();']");
+baseFunc.select(By GO_BTN = By.xpath(".//span[@class = 'gogogo']"););
+
+
+
+
+
+
+
 
     }
 }
